83882b5102b61ce67c4ccb12e25aa0f4ea71565e
gunnarverhulst <verhulstg@hotmail.com>
2023-10-26 19:26:45 +0200

    commit

--- java/src/main/java/io/tripled/elevator/ElevatorController.java
+++ java/src/main/java/io/tripled/elevator/ElevatorController.java
@@ -35,10 +35,7 @@ public class ElevatorController {
 
         while(!elevatorCalls.isEmpty()){
             ElevatorCall firstElevatorCall = getFirstElevatorCall();
-            if(currentElevatorFloor != firstElevatorCall.callOrigin()){
-                elevatorCalls.add(0,firstElevatorCall);
-                firstElevatorCall = new ElevatorCall(currentElevatorFloor, firstElevatorCall.callOrigin());
-            }
+
             List<ElevatorCall> processableCalls = new ArrayList<>();
             processableCalls.add(firstElevatorCall);
             processableCalls.addAll(scanForProcessableCalls(firstElevatorCall));
